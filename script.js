7804b3fa74b00d5561e28c84cc5e7ff1ee2d5921
Braham1234 <143471590+Braham1234@users.noreply.github.com>
2024-04-27 20:03:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'efd335dd43mshfff338499700d39p1c1c94jsn033acb1082fe',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+const getWeather=(city)=>{
+    cityName.innerHTML=city
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
+    .then(response => response.json())
+    .then((response) => {
+        console.log(response)
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        humidity1.innerHTML = response.humidity
+        max_temp.innerHTML = response.max_temp
+        min_temp.innerHTML = response.min_temp
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+        temp.innerHTML = response.temp
+        temp1.innerHTML = response.temp
+        wind_degrees.innerHTML = response.wind_degrees
+        wind_speed.innerHTML = response.wind_speed
+        wind_speed1.innerHTML = response.wind_speed
+    })
+    .catch(err => console.error(err));
+}
+submit.addEventListener("click", (e)=>{
+    e.preventDefault()
+    getWeather(city.value)
+})
+getWeather("Delhi")
